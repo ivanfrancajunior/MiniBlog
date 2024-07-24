87e11913867e6bba39ed0faf3cde3b1c707040bd
ivan franca <contato.ivanfrancajr@gmail.com>
2024-07-24 16:17:45 -0300

    fix/ refact PostDetail layout

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,39 +1,40 @@
 import { Link } from "react-router-dom";
 
 interface Props {
-  post: {
-    title: string;
-    createdBy: string;
-    tags: string[];
-    image: string;
-    id: string;
-  };
+    post: {
+        title: string;
+        createdBy: string;
+        tags: string[];
+        image: string;
+        id: string;
+    };
 }
 
 const PostDetail = ({ post }: Props) => {
-  return (
-    <div className='w-96 border-2 hover:shadow-[8px_8px_0px_rgba(0,0,0,1)] grid place-content-center  md:w-[540px] mb-4 '>
-      <img className='' src={post?.image} alt={post?.title} />
-      <div className='p-4'>
-        <Link to={`/posts/${post?.id}`}>
-          <h2 className='mb-2 text-2xl font-semibold hover:text-[#bfff00]'>
-            {post?.title}
-          </h2>
-          <p className='text-xl mb-4 '>
-            <span className='text-purple-400 '>@{post?.createdBy}</span>
-          </p>
-        </Link>
-        <div className='mb-3 text-red-500 '>
-            {post?.tags?.map((tag) => (
-              <p key={tag} className='mr-2'>
-                <span className='font-bold'>#</span>
-                {tag}
-              </p>
-            ))}
-          </div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-96 border-2 hover:shadow-[8px_8px_0px_rgba(0,0,0,1)] grid place-content-center  md:w-[540px] mb-4 ">
+            <img className="" src={post?.image} alt={post?.title} />
+            <div className="p-4">
+                <Link to={`/posts/${post?.id}`}>
+                    <h2 className="mb-2 text-2xl font-semibold hover:text-[#bfff00]">
+                        {post?.title}
+                    </h2>
+                    <p className="text-xl mb-4 font-bold group">
+                        <span className=" group-hover:text-[#bfff00]">@</span>
+                        {post?.createdBy}
+                    </p>
+                </Link>
+                <div className="flex pb-3 ">
+                    {post?.tags?.map((tag) => (
+                        <p key={tag} className="mr-2 font-bold hover:text-[#ea00ff]">
+                            <span className="">#</span>
+                            {tag}
+                        </p>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PostDetail;
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,44 +3,49 @@ import { useFetchDocument } from "../hooks/useFetchDocument";
 import Loader from "../components/Loader";
 
 const Post = () => {
-  const { id } = useParams();
-  const { document: post, loading } = useFetchDocument("posts", id);
-  return (
-    <div className='flex  flex-col container items-center justify-start gap-4  min-h-screen h-auto'>
-      <h1 className='text-5xl text-center mt-16'>Posts</h1>
-      {loading && (
-        <>
-          <Loader />
-        </>
-      )}
+    const { id } = useParams();
+    const { document: post, loading } = useFetchDocument("posts", id);
+    return (
+        <div className="flex  flex-col container items-center justify-start gap-4  min-h-screen h-auto">
+            <h1 className="text-5xl text-center mt-16">Posts</h1>
+            {loading && (
+                <>
+                    <Loader />
+                </>
+            )}
 
-      {post && (
-        <div>
-          <h1 className='text-3xl text-center my-4 font-bold'>
-            <span className="text-[#bfff00] text-4xl  font-bolder">{post?.title[0].toUpperCase()}</span>
-            {post.title.slice(1, -1)}
-          </h1>
-          <img
-            src={post.image}
-            alt={post.image}
-            className='min-w-[300px] max-w-[95%] h-auto mb-2 px-5'
-          />
-          <p className=' text-2xl text-center italic'> {post.body} </p>
-          <h3 className='my-2 text-xl font-bold'>Tags:</h3>
+            {post && (
+                <div>
+                    <h1 className="text-3xl text-center my-4 font-bold">
+                        <span className="text-[#bfff00] text-4xl  font-bolder">
+                            {post?.title[0].toUpperCase()}
+                        </span>
+                        {post.title.slice(1, post.title.length)}
+                    </h1>
+                    <img
+                        src={post.image}
+                        alt={post.image}
+                        className="min-w-[300px] max-w-[95%] h-auto mb-2 px-5"
+                    />
+                    <p className=" text-2xl text-center italic">
+                        {" "}
+                        {post.body}{" "}
+                    </p>
+                    <h3 className="my-2 text-xl font-bold">Tags:</h3>
 
-          <div className='flex gap-4'>
-            {post?.tags?.map((tag) => (
-              <div key={tag}>
-                {" "}
-                <span className='text-[#bfff00]'>#</span>
-                {tag}{" "}
-              </div>
-            ))}
-          </div>
+                    <div className="flex gap-4">
+                        {post?.tags?.map((tag) => (
+                            <div key={tag}>
+                                {" "}
+                                <span className="text-[#bfff00]">#</span>
+                                {tag}{" "}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Post;
